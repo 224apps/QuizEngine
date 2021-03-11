6c7ebcefcd0c2207524dac04253f0d89e88b155a
224Apps <224apps@gmail.com>
2021-03-11 13:26:57 -0500

    Finishing the Quiz implementation!

---
+++ QuizEngine/Question.swift
@@ -0,0 +1,41 @@
+//
+//  Question.swift
+//  QuizApp
+//
+//  Created by Abdoulaye Diallo on 2/8/21.
+//
+
+import Foundation
+
+
+public enum Question<T: Hashable>: Hashable {
+    case singleAnswer(T)
+    case multipleAnswer(T)
+    
+//    var hashValue: Int {
+//        switch self {
+//            case .singleAnswer(let value):
+//                return value.hashValue
+//            case .multipleAnswer(let value):
+//                return value.hashValue
+//        }
+//    }
+    
+    public func hash(into hasher: inout Hasher) {
+        switch self {
+            case .singleAnswer(let value): hasher.combine(value)
+            case .multipleAnswer(let value):  hasher.combine(value)
+        }
+    }
+    
+   public static func ==(lhs: Question<T>, rhs: Question<T>) -> Bool {
+        switch (lhs, rhs){
+            case (.singleAnswer(let a), .singleAnswer(let b)):
+                return a == b
+            case (.multipleAnswer(let a), .multipleAnswer(let b)):
+                return a == b
+            default:
+                return false
+        }
+    }
+}
